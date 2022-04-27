a32be06af3b6c1d6bf37fd20f29bcd04c0ff1e8d
Spelljinxer <35420739+Spelljinxer@users.noreply.github.com>
2022-04-27 20:24:26 +0800

    updated header

--- rake-p.py
+++ rake-p.py
@@ -1,3 +1,9 @@
+# CITS3002 2022 Sem 1 - Project
+# authors
+#   - Daivik Anil 22987816
+#   - Nathan Eden 22960674
+#   - Reiden Rufin 22986337
+
 rakefile_array = []
 with open('Rakefile', 'r') as f:
     for line in f:
