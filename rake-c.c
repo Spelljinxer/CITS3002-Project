78a8cdc43bf970e7d2b3a115794b68466bdecc04
Spelljinxer <35420739+Spelljinxer@users.noreply.github.com>
2022-04-27 20:22:58 +0800

    can now read the rakefile

--- rake-c.c
+++ rake-c.c
@@ -1,22 +1,43 @@
+/** 
+ *  CITS3002 2022 Sem 1 - Project
+ * @authors
+ *  - Daivik Anil 22987816
+ *  - Nathan Eden 22960674
+ *  - Reiden Rufin 22986337
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-
+//#define MAX_LINE_LENGTH 1024
 
 //-------------------------------------------------------------------------
 
 
 void read_rakefile(char *rakefile)
 {
-  FILE *fp;
-  
+    int c;
+    FILE *fptr;
+
+    if ((fptr = fopen(rakefile, "r")) == NULL)
+    {
+        printf("Error reading file\n");
+    }
+    else
+    {
+        while ((c = getc(fptr)) != EOF)
+        {
+            printf("%c", c);
+        }
+        fclose(fptr);
+    }
 }
 
 
 int main(int argc, char* argv[])
 {
-    printf("Hello World\n");
+    read_rakefile(argv[1]);
     return 0;
 }
 
