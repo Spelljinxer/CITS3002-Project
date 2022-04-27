7ccb68fc2d89f1cd7cbdc9af2bda29cd3934b4ea
Spelljinxer <35420739+Spelljinxer@users.noreply.github.com>
2022-04-27 20:05:44 +0800

    upload of client C

---
+++ rake-c.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+//-------------------------------------------------------------------------
+
+
+void read_rakefile(char *rakefile)
+{
+  FILE *fp;
+  
+}
+
+
+int main(int argc, char* argv[])
+{
+    printf("Hello World\n");
+    return 0;
+}
+
