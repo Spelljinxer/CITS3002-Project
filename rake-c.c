b1a6488bcab45a1a6d0b8ae632ed40e86e2acca3
Spelljinxer <35420739+Spelljinxer@users.noreply.github.com>
2022-05-04 21:01:04 +0800

    attempted to append 3d array

--- rake-c.c
+++ rake-c.c
@@ -12,12 +12,14 @@
 #include <stdbool.h>
 #include "strsplit.c"
 
+
 #define BUFFSIZE 1024
 
 //-------------------------------------------------------------------------
 
 char buffer[BUFFSIZE];
 
+char* arr[BUFFSIZE][BUFFSIZE];
 
 void read_rakefile(char *rakefile)
 {
@@ -28,23 +30,32 @@ void read_rakefile(char *rakefile)
         printf("Error opening file!\n");
         exit(1);
     }
-   
+    
     int nwords;
+
     // https://stackoverflow.com/questions/56226129/how-to-skip-a-comment-in-c-programming-with-using-fopen
     while (fgets (buffer, BUFFSIZE, fptr)) {   /* read every line */
         buffer[strcspn (buffer, "#\r\n")] = 0;  /* trim comment or line-ending */
-        //puts (buffer);                      /* output line w/o comment - replace this later when we add it to a DS */
-
         char **words = strsplit(buffer, &nwords);
         for(int w=0 ; w<nwords ; ++w) 
         {
-            //check if the current word is a tab ident
-            if(words[w][0] == '\v')
+            //place PORT and port number in array
+            if(strcmp(words[w], "PORT") == 0)
             {
-                printf("%s\n", words[w]);
+                //places "PORT" in first column
+                arr[0][w] = words[w];
+                //places port number in second column
+                arr[1][w] = words[w+2];
             }
-        }
+            //place HOST inside third column
+            else if(strcmp(words[w], "HOST") == 0)
+            {
+                arr[2][w] = words[w];
+                //place host name inside fourth column
+                arr[3][w] = words[w+2];
+            }
 
+        }
     }
     fclose(fptr);
 }
