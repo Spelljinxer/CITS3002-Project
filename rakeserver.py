24c16eef070bfc1b2adc6b1e19156af5b32e4325
Spelljinxer <35420739+Spelljinxer@users.noreply.github.com>
2022-05-11 16:54:06 +0800

    upload of server

---
+++ rakeserver.py
@@ -0,0 +1,30 @@
+# CITS3002 2022 Sem 1 - Project - rakeserver
+# authors
+#   - Daivik Anil 22987816
+#   - Nathan Eden 22960674
+#   - Reiden Rufin 22986337
+#--------------------------------------------------------------------------
+
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+port = 6238 #going to need a way to change this so its dynamic with the client's Rakefiles
+sock.bind(('', port))
+print("Listening on port " + str(port))
+
+sock.listen(5)
+
+while True:
+    c, address = sock.accept()
+    print("connected from:", address)
+
+    c.send("Hello, client!".encode())
+    #receive message from client and decode it
+    data = c.recv(1024)
+    data = data.decode()
+    print("Received:", data)
+
+    c.close()
+    break
+
